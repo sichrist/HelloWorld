4fde279eaa15131eb3a8a2ffd7bdbe8e108613b9
Simon Christofzik <si241chr@htwg-konstanz.de>
2018-08-10 11:49:06 +0200

    updated jenkinsfile

--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -7,6 +7,9 @@ int main()
     std::cout << "Hello World" << std::endl;
     Num num = Num(5);
     std::cout << "Num "<<num.getNum()<<std::endl;
-
+    
+    
+    std::cout << "Hello World" << std::endl;
+    std::cout << "Num "<<num.getNum()<<std::endl;
 }
 
