fd0ac732c27f38c371bd5a24f37f273d7faf2f70
Simon Christofzik <si241chr@htwg-konstanz.de>
2018-08-10 11:51:40 +0200

    updated jenkinsfile

--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 #include "testheader.h"
 
+void test_method()
+{
+	int a = 0;
+	for (int i = 0; i < 10; ++i)
+	{
+		a = i + a;
+	}
+}
+
 int main()
 {
 
     std::cout << "Hello World" << std::endl;
     Num num = Num(5);
     std::cout << "Num "<<num.getNum()<<std::endl;
-    
+
     
     std::cout << "Hello World" << std::endl;
     std::cout << "Num "<<num.getNum()<<std::endl;
+
+    test_method();
+
+    	int a = 0;
+	for (int i = 0; i < 10; ++i)
+	{
+		a = i + a;
+	}
 }
 
