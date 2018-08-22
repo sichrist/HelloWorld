e82dfde594937abe073e9a13e5f1744c802e4210
Simon Christofzik <si241chr@htwg-konstanz.de>
2018-08-22 22:16:51 +0200

    updated

--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -3,12 +3,12 @@
 
 void test_method()
 {
-	int a = 0;
+    int a = 0;
 for (int i = 0; i < 10; ++i)
-	{
+    {
 a = i + a;
 }
-	}
+    }
 
 int main()
 {
--- test/testheader_test.cpp
+++ test/testheader_test.cpp
@@ -3,22 +3,22 @@
 
 class TestHeaderTest : public::testing::Test {
 protected:
-	virtual void SetUp() {
+    virtual void SetUp() {
 
-	}
+    }
 
-	virtual void TearDown() {
+    virtual void TearDown() {
 
-	}
+    }
 
 };
 
 TEST_F(TestHeaderTest,getNum){
-	const int x = 4;
-	Num num = Num(x);
-	Num num1 = Num();
-	EXPECT_EQ(x,num.getNum());
-	EXPECT_EQ(0,num1.getNum());
+    const int x = 4;
+    Num num = Num(x);
+    Num num1 = Num();
+    EXPECT_EQ(x,num.getNum());
+    EXPECT_EQ(0,num1.getNum());
 }
 
-	
+    
